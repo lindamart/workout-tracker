7bc7fa6bc2f9b3be82c899b81a7d24b131de44bf
Linda Martinez <lindanmjw@gmail.com>
2022-01-15 10:12:52 -0500

    adding get and put code

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,27 +1,39 @@
 const router = require("express").Router();
-const Transaction = require("../models/transaction.js");
+const Workout = require("../models/workoutSchema.js");
 
-router.post("/api/transaction", ({ body }, res) => {
-    Transaction.create(body)
-        .then(dbTransaction => {
-            res.json(dbTransaction);
+router.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
         })
         .catch(err => {
             res.status(400).json(err);
         });
 });
 
-router.post("/api/transaction/bulk", ({ body }, res) => {
-    Transaction.insertMany(body)
-        .then(dbTransaction => {
-            res.json(dbTransaction);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
+// router.post("/api/transaction/bulk", ({ body }, res) => {
+//     Transaction.insertMany(body)
+//         .then(dbTransaction => {
+//             res.json(dbTransaction);
+//         })
+//         .catch(err => {
+//             res.status(400).json(err);
+//         });
+// });
+
+// update below with workout info
+router.put("/api/workouts/:id", (req, res) => {
+    Workout.findByIdAndUpdate(req.params.id, {$push:{exercises:req.body}},{new:true, runValidators:true})
+    .then(dbTransaction => {
+        res.json(dbTransaction);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
 });
 
-router.get("/api/transaction", (req, res) => {
+
+router.get("/api/workouts", (req, res) => {
     Transaction.find({})
         .sort({ date: -1 })
         .then(dbTransaction => {
@@ -31,5 +43,8 @@ router.get("/api/transaction", (req, res) => {
             res.status(400).json(err);
         });
 });
+
+// add >>>/api/workouts/range
+// delete??
 
 module.exports = router;
