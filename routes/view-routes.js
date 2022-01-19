e124560d49928dc99b89745c31340df9cdfadb0f
Linda Martinez <lindanmjw@gmail.com>
2022-01-18 19:11:25 -0500

    add sendfile for index exercise and stats.html

--- routes/view-routes.js
+++ routes/view-routes.js
@@ -3,14 +3,28 @@ const path = require("path")
 module.exports = (app)=>{
 
 // youre going to want to sendFile** for the index.html, exercise.html, stats.html
-
 // reference module 11 homework Note Taker on how to do this
 // module 11 sample 
 // app.get('/routes', (req, res) =>
 //   res.sendFile(path.join(__dirname, 'public/routes.html'))
 //   );
 
-
-// after this is completed and you can see all your html's then move to getting API routes working
+// SendFile for the index.html
+router.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+// SendFile exercise.html
+router.get("/exercise", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+// SendFile stats.html
+router.get("/stats", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
 
 }
+
+// do we still need module.exports = router with above format??
+
+
+// after this is completed and you can see all your html's then move to getting API routes working
